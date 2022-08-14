e67fdf9b3cbd9e8cf9c95b0f38bf83f715f1fa3b
JackR42 <j.roumimper@gmail.com>
2022-08-14 22:57:13 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+# variables.tf
+# Generic variables
+variable "env" {
+    description = "The name of the environment"
+}
+variable "resource-group-name" {
+    description = "The name of the resource group"
+}
+variable "location-name" {
+  description = "The name of the Azure Region in which all resources should be created."
+}
