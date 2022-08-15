079a91eff676ba466072d74d59a24bc044b5309f
JackR42 <j.roumimper@gmail.com>
2022-08-15 15:46:09 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 # variables.tf
+
 # Generic variables
 variable "env" {
     description = "The name of the environment"
@@ -8,4 +9,15 @@ variable "resource-group-name" {
 }
 variable "location-name" {
   description = "The name of the Azure Region in which all resources should be created."
+}
+
+# Database variables
+variable "database-instance-name" {
+    description = "The name of the SQL Server Instance"
+}
+variable "database-instance-name-fqdn" {
+    description = "The FQDN name of the SQL Server Instance"
+}
+variable "database-database1-name" {
+    description = "The name of the first  SQL Server database"
 }
